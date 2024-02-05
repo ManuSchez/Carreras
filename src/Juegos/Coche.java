a08ff5bba135e59f55d371aab62262d5d82aa3d2
manuel.sanchez <manuelsschez@gmail.com>
2024-02-05 12:08:09 +0100

    Commit final

--- src/Juegos/Coche.java
+++ src/Juegos/Coche.java
@@ -1,5 +1,47 @@
 package Juegos;
 
-public class Coche {
+import java.util.Scanner;
 
-}
+public class Coche {
+    private static String nombre;
+    private static Reloj tiempoDeVuelta;
+    public static void datosCoche(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("¿Cuantos coches correrán la carrera?");
+        int num = sc.nextInt();
+        sc.nextLine();
+
+        String[] nombre = new String[num];
+        int[] velocidad = new int[num];
+        for (int i = 0; i < num; i++) {
+            System.out.println("Introduce el nombre del coche número " + (i + 1) + ":");
+            nombre[i] = sc.nextLine();
+            System.out.println("Introduce la velocidad máxima del coche número " + (i + 1) + ":");
+            velocidad[i] = sc.nextInt();
+            sc.nextLine(); // Consumir el salto de línea pendiente
+        }
+        System.out.println("Coches que correrán la carrera:");
+        for (int i = 0; i < num; i++) {
+            System.out.println("Correrá: " + nombre[i] + " con velocidad máxima de " + velocidad[i] + " km/h");
+        }
+
+    }
+    public static void datosCircuito() {
+        System.out.println("Circuito: " + Reloj.getCircuito());
+        System.out.println("Horas de vuelta: "+ Reloj.getHora());
+        System.out.println("Minutos de vuelta: "+ Reloj.getMinutos());
+        System.out.println("Segundos de vuelta: " + Reloj.getSegundos());
+    }
+
+    public  Coche(String nombre, int horas, int minutos, int segundos1) {
+        this.nombre = nombre;
+        this.tiempoDeVuelta = new Reloj(horas, minutos, segundos1);
+    }
+        public String toString() {
+            return "Coche: " + nombre + ", Tiempo de vuelta: " + tiempoDeVuelta.toString();
+        }
+    }
+
+
+
+
--- src/Juegos/Reloj.java
+++ src/Juegos/Reloj.java
@@ -1,47 +1,39 @@
 package Juegos;
 
+import java.time.LocalTime;
 import java.util.Scanner;
 
 public class Reloj {
 
-    private int hora;
-    private int minutos;
-    private int segundos;
+    private static int hora;
+    private static int minutos;
+    private static int segundos;
+    private static String circuito;
     Scanner sc = new Scanner(System.in);
+
     public void setReloj(int hora, int minutos, int segundos) {
         this.hora = 0;
         this.minutos = 0;
         this.segundos = 0;
     }
+
     public Reloj(int hora, int minutos, int segundos) {
         if (hora >= 0 && hora <= 23 && minutos >= 0 && minutos <= 59 && segundos >= 0 && segundos <= 59) {
             this.hora = hora;
             this.minutos = minutos;
             this.segundos = segundos;
-        } else {
-            System.out.println("Hora, minutos o segundos inválidos.");
-            // Por defecto, inicializamos la hora a las 00:00:00
-            this.hora = 0;
-            this.minutos = 0;
-            this.segundos = 0;
         }
     }
-    public boolean modificarHora(int hora, int minutos, int segundos) {
-        System.out.println("Introduce la hora: ");
+    public static void datosVuelta(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce nombre del circuito: ");
+        circuito = sc.nextLine();
+        System.out.println("Introduce horas de tiempo de vuelta para clasificar: ");
         hora = sc.nextInt();
-        System.out.println("Introduce los minutos: ");
+        System.out.println("Introduce minutos de vuelta para clasificar: ");
         minutos = sc.nextInt();
-        System.out.println("Introduce los segundos: ");
+        System.out.println("Introduce segundos de vuelta para clasificar: ");
         segundos = sc.nextInt();
-        if (hora >= 0 && hora <= 23 && minutos >= 0 && minutos <= 59 && segundos >= 0 && segundos <= 59) {
-            this.hora = hora;
-            this.minutos = minutos;
-            this.segundos = segundos;
-            return true;
-        } else {
-            System.out.println("Hora, minutos o segundos inválidos.");
-            return false;
-        }
     }
     public void sumarSegundo() {
         segundos++;
@@ -57,7 +49,41 @@ public class Reloj {
             }
         }
     }
+    public static String getCircuito() {
+        return circuito;
+    }
+    public static int getHora() {
+        return hora;
+    }
+
+    public static int getMinutos() {
+        return minutos;
+    }
+
+    public static int getSegundos() {
+        return segundos;
+    }
+    public static void cuentaAtras() {
+        int segundos = 6;
+        LocalTime horaActual = LocalTime.now();
+        System.out.println("Hora actual: " + horaActual);
+
+        Reloj reloj =  new Reloj (24 , 60, 60);
+        while (segundos > 0) {
+            System.out.println(segundos);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            segundos--;
+
+        }
+        System.out.println("¡Comienza la carrera!");
+    }
+
     public String toString() {
         return String.format("%02d:%02d:%02d", hora, minutos, segundos);
     }
 }
+
